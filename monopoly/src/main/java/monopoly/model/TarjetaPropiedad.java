0d651a506dbbdb00af796080da8b6d902663d0ed
pablo.moreno.la31@gmail.com <pablo.moreno.la31@gmail.com@dedf9ac4-86ca-401c-2757-c7f10075d506>
2013-04-13 03:12:54 +0000

    - Nuevos atributos a algunas clases.

--- monopoly/src/main/java/monopoly/model/TarjetaPropiedad.java
+++ monopoly/src/main/java/monopoly/model/TarjetaPropiedad.java
@@ -13,6 +13,11 @@ public abstract class TarjetaPropiedad {
     
     private Jugador jugador;
     
+    private Integer idTarjeta;
+    
+    private String descripcion;
+    
+    private Integer valorHipotecario;
     
     public TarjetaPropiedad(){
 	
@@ -31,7 +36,47 @@ public abstract class TarjetaPropiedad {
     public void setJugador(Jugador jugador) {
         this.jugador = jugador;
     }
-    
-    
 
+    /**
+     * @return the idTarjeta
+     */
+    public Integer getIdTarjeta() {
+        return idTarjeta;
+    }
+
+    /**
+     * @param idTarjeta the idTarjeta to set
+     */
+    public void setIdTarjeta(Integer idTarjeta) {
+        this.idTarjeta = idTarjeta;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the valorHipotecario
+     */
+    public Integer getValorHipotecario() {
+        return valorHipotecario;
+    }
+
+    /**
+     * @param valorHipotecario the valorHipotecario to set
+     */
+    public void setValorHipotecario(Integer valorHipotecario) {
+        this.valorHipotecario = valorHipotecario;
+    }
+    
 }
