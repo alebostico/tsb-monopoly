e0b5563cddca9aae195c013c6745fb5ddc1ec975
alebostico@gmail.com <alebostico@gmail.com@dedf9ac4-86ca-401c-2757-c7f10075d506>
2013-09-13 22:04:21 +0000

    Casillero exception, no se usa todavia

---
+++ monopoly/src/main/java/monopoly/exception/CasilleroException.java
@@ -0,0 +1,33 @@
+package monopoly.exception;
+
+public class CasilleroException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8878397796894579644L;
+
+	public CasilleroException() {
+		super();
+	}
+
+	public CasilleroException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public CasilleroException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public CasilleroException(String message) {
+		super(message);
+	}
+
+	public CasilleroException(Throwable cause) {
+		super(cause);
+	}
+
+	
+	
+}
