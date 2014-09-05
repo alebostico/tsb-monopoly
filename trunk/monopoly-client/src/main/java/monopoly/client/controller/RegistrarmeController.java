8746fe52d599996ad8bbf5e3f778e502cd6ff701
pablo.moreno.la31@gmail.com <pablo.moreno.la31@gmail.com@dedf9ac4-86ca-401c-2757-c7f10075d506>
2014-09-05 19:40:48 +0000

    - Ventana de Registrar Usuario

--- trunk/monopoly-client/src/main/java/monopoly/client/controller/RegistrarmeController.java
+++ trunk/monopoly-client/src/main/java/monopoly/client/controller/RegistrarmeController.java
@@ -12,55 +12,56 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * @author pablo
  *
  */
-public class RegistrarmeController implements Initializable {
+public class RegistrarmeController extends AnchorPane implements Initializable {
 
-	 @FXML
-	    private Button btnSalir;
+	@FXML
+	private Button btnSalir;
 
-	    @FXML
-	    private TextField txtNombre;
+	@FXML
+	private TextField txtNombre;
 
-	    @FXML
-	    private TextField txtUserName;
+	@FXML
+	private TextField txtUserName;
 
-	    @FXML
-	    private Button btnGuardar;
+	@FXML
+	private Button btnGuardar;
 
-	    @FXML
-	    private PasswordField txtRepeatPassword;
+	@FXML
+	private PasswordField txtRepeatPassword;
 
-	    @FXML
-	    private PasswordField txtPassword;
+	@FXML
+	private PasswordField txtPassword;
 
-	    @FXML
-	    void processComparateFields(ActionEvent event) {
+	@FXML
+	void processComparateFields(ActionEvent event) {
+		
+	}
 
-	    }
+	@FXML
+	void processExit(ActionEvent event) {
 
-	    @FXML
-	    void processExit(ActionEvent event) {
+	}
 
-	    }
+	@FXML
+	void processSave(ActionEvent event) {
 
-	    @FXML
-	    void processSave(ActionEvent event) {
+	}
 
-	    }
-	    
-	/* (non-Javadoc)
-	 * @see javafx.fxml.Initializable#initialize(java.net.URL, java.util.ResourceBundle)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javafx.fxml.Initializable#initialize(java.net.URL,
+	 * java.util.ResourceBundle)
 	 */
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-
 	}
-	
-	
 
 }
