e7fa78fa537c69529d0520f0d3a0a1f473f39c2d
pablo.moreno.la31@gmail.com <pablo.moreno.la31@gmail.com@dedf9ac4-86ca-401c-2757-c7f10075d506>
2014-09-05 17:45:48 +0000

    - Agregado de ventanas nuevas

--- monopoly-client/src/main/java/monopoly/client/controller/LoginController.java
+++ monopoly-client/src/main/java/monopoly/client/controller/LoginController.java
@@ -3,12 +3,15 @@
  */
 package monopoly.client.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -16,6 +19,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import monopoly.client.connection.TCPClient;
 import monopoly.client.gui.FXMLIniciarSesion;
+import monopoly.client.util.ScreensFramework;
 import monopoly.message.impl.LoginMensaje;
 import monopoly.util.GestorLogs;
 import monopoly.util.encriptacion.Encrypter;
@@ -123,6 +127,38 @@ public class LoginController extends AnchorPane implements Initializable {
 		} else {
 			cliente.detenerHilo();
 			System.exit(0);
+		}
+	}
+	
+	public void processRegister(ActionEvent event)
+	{
+		Parent root;
+		RegistrarmeController controller;
+		String fxml = "/fxml/Registrarme.fxml";
+		
+		if (APPLICATION == null) {
+			// We are running in isolated FXML, possibly in Scene Builder.
+			// NO-OP.
+			errorMessage.setText("Se produjo un error al iniciar sesión");
+		} else {
+			try {
+				root = ScreensFramework.getParent(fxml);
+				
+				controller = (RegistrarmeController) ScreensFramework
+						.getController(fxml);
+				
+				APPLICATION.getPrimaryStage().setScene(new Scene(root));
+				APPLICATION.getPrimaryStage().centerOnScreen();
+				APPLICATION.getPrimaryStage().setTitle("Monopoly - Registrar Usuario");
+				APPLICATION.getPrimaryStage().show();
+
+			} catch (IOException ex) {
+				// TODO Auto-generated catch block
+				GestorLogs.registrarError(ex.getMessage());
+			} catch (Exception ex) {
+				// TODO Auto-generated catch block
+				GestorLogs.registrarError(ex.getMessage());
+			}
 		}
 	}
 
