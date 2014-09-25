85a570c714231c38d9be67ee03e7176d5f4288b2
pablo.moreno.la31@gmail.com <pablo.moreno.la31@gmail.com@dedf9ac4-86ca-401c-2757-c7f10075d506>
2014-09-25 07:55:27 +0000

    - Nueva excepción - Constantes de los files fxml - Lector Fxml

---
+++ monopoly-util/src/main/java/monopoly/util/ConstantesFXML.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package monopoly.util;
+
+/**
+ * @author Bostico Alejandro
+ * @author Moreno Pablo
+ *
+ */
+public class ConstantesFXML {
+
+	public final static String FXML_INICIAR_SESION = "/fxml/IniciarSesion.fxml";
+	public final static String FXML_MENU_OPCIONES = "/fxml/MenuOpciones.fxml";
+	public final static String FXML_REGISTRARME = "/fxml/Registrarme.fxml";
+	public final static String FXML_CREAR_JUEGO = "/fxml/CrearJuego.fxml";
+	public final static String FXML_UNIRME_JUEGO = "/fxml/UnirmeJuego.fxml";
+	
+}
