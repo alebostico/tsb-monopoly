6c550d87f1803d9bd21cc2f65e3cd3b102dcbe98
pablo.moreno.la31@gmail.com <pablo.moreno.la31@gmail.com@dedf9ac4-86ca-401c-2757-c7f10075d506>
2014-10-18 13:39:28 +0000

    - Gestion de mensajes para el envío  y recepción entre cliente y servidor.

---
+++ trunk/monopoly-util/src/main/java/monopoly/util/message/ResetSignal.java
@@ -0,0 +1,16 @@
+package monopoly.util.message;
+
+/**
+ * @author Bostico Alejandro
+ * @author Moreno Pablo
+ *
+ */
+
+/**
+ * This package private class is used internally in Hub and Client
+ * to send a signal to the message output thread that the output
+ * stream should be reset.  A ResetSignal has no data.
+ */
+public class ResetSignal {
+
+}
