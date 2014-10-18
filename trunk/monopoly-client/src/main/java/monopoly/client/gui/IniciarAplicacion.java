76555e4618cb0cc945df0487602e8f2d91007e83
pablo.moreno.la31@gmail.com <pablo.moreno.la31@gmail.com@dedf9ac4-86ca-401c-2757-c7f10075d506>
2014-10-18 13:42:52 +0000

    - Nueva gestión de conexión al servidor. - Corrección  de  Stage.

--- trunk/monopoly-client/src/main/java/monopoly/client/gui/IniciarAplicacion.java
+++ trunk/monopoly-client/src/main/java/monopoly/client/gui/IniciarAplicacion.java
@@ -6,12 +6,11 @@ package monopoly.client.gui;
 import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.stage.Stage;
-import monopoly.client.connection.ConexionController;
 import monopoly.client.controller.LoginController;
 import monopoly.client.util.ScreensFramework;
 import monopoly.util.GestorLogs;
+import monopoly.util.constantes.ConstantesFXML;
 
 /**
  * 
@@ -19,10 +18,6 @@ import monopoly.util.GestorLogs;
  *
  */
 public class IniciarAplicacion extends Application {
-
-	private Stage primaryStage;
-	
-	private LoginController loginController;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -34,30 +29,18 @@ public class IniciarAplicacion extends Application {
 	 * @see javafx.application.Application#start(javafx.stage.Stage)
 	 */
 	@Override
-	public void start(Stage stage) throws IOException {
+	public void start(final Stage stage) throws IOException {
 		// TODO Auto-generated method stub
-		primaryStage = stage;
-		
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				Platform.runLater(new Runnable() {
-					@Override
-					public void run() {
-						ConexionController.iniciarConexion();
-						gotoLogin();
-					}
-				});
-			}
-		}).start();
+		gotoLogin(stage);
 	}
 
-	private void gotoLogin() {
-		String fxml = "/fxml/IniciarSesion.fxml";
+	private void gotoLogin(Stage primaryStage) {
+		String fxml = ConstantesFXML.FXML_INICIAR_SESION;
+		LoginController loginController;
 		try {
 			loginController = (LoginController) ScreensFramework
 					.replaceSceneContent(fxml, primaryStage);
-			
+
 			loginController.setPrimaryStage(primaryStage);
 			primaryStage.setTitle("Monopoly - Iniciar Sesión");
 			primaryStage.show();
@@ -66,34 +49,4 @@ public class IniciarAplicacion extends Application {
 			GestorLogs.registrarError(ex.getMessage());
 		}
 	}
-
-	/**
-	 * @return the primaryStage
-	 */
-	public Stage getPrimaryStage() {
-		return primaryStage;
-	}
-
-	/**
-	 * @param primaryStage the primaryStage to set
-	 */
-	public void setPrimaryStage(Stage primaryStage) {
-		this.primaryStage = primaryStage;
-	}
-
-	/**
-	 * @return the loginController
-	 */
-	public LoginController getLoginController() {
-		return loginController;
-	}
-
-	/**
-	 * @param loginController the loginController to set
-	 */
-	public void setLoginController(LoginController loginController) {
-		this.loginController = loginController;
-	}
-	
-	
 }
