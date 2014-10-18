649c39e973dfdaebf1dbe04dfb481ad652e5cbf8
pablo.moreno.la31@gmail.com <pablo.moreno.la31@gmail.com@dedf9ac4-86ca-401c-2757-c7f10075d506>
2014-10-18 21:02:06 +0000

    - Creación  de una constante.

--- monopoly-util/src/main/java/monopoly/util/constantes/ConstantesMensaje.java
+++ monopoly-util/src/main/java/monopoly/util/constantes/ConstantesMensaje.java
@@ -11,13 +11,14 @@ public class ConstantesMensaje {
 	
 	public static String DELIMITADOR = "&-&-&";
 	public static String DELIMITADOR_OBJETOS = "&_&_&";
-
+	
+	public final static String CREAR_JUEGO = "CreateGame";
+	
 	public final static String CLOSE_GAME = "monopoly.message.impl.SalirJuegoMensaje";
 	public final static String LOGIN = "monopoly.message.impl.LoginMensaje";
 	public final static String LOGIN_RESULTADO = "monopoly.client.message.impl.LoginResultadoMensaje";
 	public final static String REGISTRARME = "monopoly.message.impl.RegistrarmeMensaje";
 	public final static String REGISTRARME_RESULTADO = "monopoly.client.message.impl.RegistrarmeResultadoMensaje";
-	public final static String CREAR_JUEGO = "monopoly.message.impl.CrearJuegoMensaje";
 	public final static String INICIAR_PARTIDA = "IniciarPartidaMensaje";
 
 }
