9a895b2a828f9c7666179dae3266c656cc6ee79f
pablo.moreno.la31@gmail.com <pablo.moreno.la31@gmail.com@dedf9ac4-86ca-401c-2757-c7f10075d506>
2014-10-29 18:07:57 +0000

    - Cambios en la lista circular. - Creación y modificación de algunos mensajes

--- trunk/monopoly-util/src/main/java/monopoly/util/constantes/ConstantesMensaje.java
+++ trunk/monopoly-util/src/main/java/monopoly/util/constantes/ConstantesMensaje.java
@@ -8,17 +8,42 @@ package monopoly.util.constantes;
  *
  */
 public class ConstantesMensaje {
-	
-	public static String DELIMITADOR = "&-&-&";
-	public static String DELIMITADOR_OBJETOS = "&_&_&";
-	
-	public final static String CREAR_JUEGO = "CreateGame";
-	
-	public final static String CLOSE_GAME = "monopoly.message.impl.SalirJuegoMensaje";
-	public final static String LOGIN = "monopoly.message.impl.LoginMensaje";
-	public final static String LOGIN_RESULTADO = "monopoly.client.message.impl.LoginResultadoMensaje";
-	public final static String REGISTRARME = "monopoly.message.impl.RegistrarmeMensaje";
-	public final static String REGISTRARME_RESULTADO = "monopoly.client.message.impl.RegistrarmeResultadoMensaje";
-	public final static String INICIAR_PARTIDA = "IniciarPartidaMensaje";
 
+	/*************************************************************************************/
+	/**
+	 * Mensajes mediante clases Message
+	 * 
+	 */
+	public final static String LOGIN_MESSAGE = "LoginMessage";
+	public final static String CREATE_ACCOUNT_MESSAGE = "CreateAccountMessage";
+	public final static String CREATE_GAME_MESSAGE = "CreateGameMessage";
+	public final static String JOIN_GAME_MESSAGE = "JoinGameMessage";
+	public final static String LOAD_GAME_MESSAGE = "LoadGameMessage";
+	public final static String START_GAME_MESSAGE = "StartGameMessage";
+	public final static String STATUS_GAME_MESSAGE = "MonopolyGameStatus";
+	
+	
+	/*************************************************************************************/
+
+	
+	/*************************************************************************************/
+	/**
+	 * Mensajes de conexión
+	 * 
+	 */
+	public final static String DISCONNECT_MESSAGE = "DisconnectMessage";
+	public final static String RESET_SIGNAL_MESSAGE = "ResetSignal";
+
+	/*************************************************************************************/
+	
+	
+	/*************************************************************************************/
+	/**
+	 * Mensajes mediante cadena de String
+	 * 
+	 */
+	public final static String GET_PENDING_GAMES_MESSAGE = "GetPendingGamesMessage";
+	public final static String THROW_DICE_TURNS_MESSAGE = "ThrowDiceTurnsMessage";
+	
+	/*************************************************************************************/
 }
