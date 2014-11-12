2361a931b252b3b9c7e1863d0b2a79d76e8f9159
alebostico@gmail.com <alebostico@gmail.com@dedf9ac4-86ca-401c-2757-c7f10075d506>
2014-11-12 22:36:05 +0000

    + added JugadorController

---
+++ trunk/monopoly/src/main/java/monopoly/controller/JugadorController.java
@@ -0,0 +1,135 @@
+package monopoly.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import monopoly.model.Dado;
+import monopoly.model.Jugador;
+import monopoly.model.JugadorHumano;
+import monopoly.model.JugadorVirtual;
+import monopoly.util.constantes.ConstantesMensaje;
+import monopoly.util.list.CircularList;
+import monopoly.util.list.Node;
+
+/**
+ * 
+ * @author Bostico Alejandro
+ * @author Moreno Pablo
+ *
+ */
+public class JugadorController {
+
+	private TreeMap<Integer, JugadorHumano> networkPlayers;
+
+	private List<Jugador> jugadoresList;
+
+	private CircularList<Jugador> turnosList;
+
+	private Node<Jugador> currentPlayer;
+
+	public JugadorController() {
+		super();
+		this.networkPlayers = new TreeMap<Integer, JugadorHumano>();
+		this.turnosList = new CircularList<Jugador>();
+		this.jugadoresList = new ArrayList<Jugador>();
+	}
+	
+	public void addPlayer(Jugador jugador) {
+		JugadorHumano jh;
+
+		if (jugador instanceof JugadorHumano) {
+			jh = (JugadorHumano) jugador;
+			if (!networkPlayers.containsKey(jh.getSenderID())) {
+				networkPlayers.put(jh.getSenderID(), (JugadorHumano) jugador);
+			}
+		}
+
+		if (!jugadoresList.contains(jugador)) {
+			jugadoresList.add(jugador);
+			turnosList.push_back(jugador);
+		}
+
+	}
+
+	public void establecerTurnos() {
+		for (Jugador jugador : jugadoresList) {
+			if (jugador instanceof JugadorVirtual)
+				jugador.setTiradaInicial(new Dado());
+			else {
+				int recipientID = ((JugadorHumano) jugador).getSenderID();
+				PartidasController
+						.getInstance()
+						.getMonopolyGame()
+						.sendToOne(recipientID,
+								ConstantesMensaje.THROW_DICE_TURNS_MESSAGE);
+			}
+		}
+	}
+	
+	public void ordenarTurnos() {
+
+		Node<Jugador> primero = (Node<Jugador>) turnosList.getHeader();
+		Node<Jugador> tmp = null;
+		Jugador aux;
+
+		do {
+			tmp = primero.getNext();
+			while (tmp != (Node<Jugador>) turnosList.getHeader()) {
+				if (primero.getKey().getTiradaInicial().getSuma() < tmp
+						.getKey().getTiradaInicial().getSuma()) {
+					aux = primero.getKey();
+					primero.setKey(tmp.getKey());
+					tmp.setKey(aux);
+
+				}
+				tmp = tmp.getNext();
+			}
+			primero = primero.getNext();
+
+		} while (tmp != (Node<Jugador>) turnosList.getHeader());
+
+		currentPlayer = (Node<Jugador>) turnosList.getHeader();
+
+	}
+	
+	public int cantJugadoresConectados() {
+		return this.jugadoresList.size();
+	}
+
+	public JugadorHumano getJugadorHumano(int key){
+		return networkPlayers.get(key);
+	}
+	
+	public List<JugadorHumano> getListaJugadoresHumanos(){
+		return (List<JugadorHumano>) networkPlayers.values();
+	}
+	
+	public List<Jugador> getTurnoslist(){
+		return turnosList.toList();
+	}
+	
+	public Jugador getCurrentPlayer(){
+		return this.currentPlayer.getKey();
+	}
+	
+	public int[] getIdConnectionClient(){
+		
+		int[] vIdConnection = new int[networkPlayers.size()];
+		
+		int index = 0;
+		
+		for (Map.Entry<Integer, JugadorHumano> entry : networkPlayers
+				.entrySet()) {
+			vIdConnection[index] = entry.getKey();
+
+			index++;
+		}
+		return vIdConnection;
+	}
+	
+	
+	
+	
+}
